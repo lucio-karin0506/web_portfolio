634e06f092b88784984a0a4c10598bbbdbe3c180
lucio <syy2585@naver.com>
2021-03-09 02:27:19 +0900

    modify index def

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,4 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from flask.helpers import url_for
+from werkzeug.utils import redirect
+
+from pybo.models import Question
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -8,4 +12,4 @@ def hello_pybo():
 
 @bp.route('/')
 def index():
-    return 'Pybo index'
+    return redirect(url_for('question._list'))
